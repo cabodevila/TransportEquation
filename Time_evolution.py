5fec45046b192d41b94cce81670bd155d4885fa2
Sergio Barrera Cabodevila <33979531+cabodevila@users.noreply.github.com>
2021-12-12 20:18:27 +0100

    Add files via upload

--- Time_evolution.py
+++ Time_evolution.py
@@ -6,6 +6,7 @@ import os
 import numpy as np
 import matplotlib.pyplot as plt
 import Lattice
+from scipy import interpolate
 
 class Evolution():
 
@@ -19,12 +20,12 @@ class Evolution():
         self.lattice.save_lattice()
 
         # Space between each element of the lattice
-        self.deltap = [self.lattice.get_lattice()[i+1] - self.lattice.get_lattice()[i]
-                       for i in range(len(self.lattice.get_lattice()) - 1)]
+        self.deltap = np.array([self.lattice.get_lattice()[i+1] - self.lattice.get_lattice()[i]
+                                for i in range(len(self.lattice.get_lattice()) - 1)])
 
         # Space between each mean value of the lattice
-        self.deltap_ = [self.lattice.get_p_lattice()[i+1] - self.lattice.get_p_lattice()[i]
-                        for i in range(len(self.lattice.get_p_lattice()) - 1)]
+        self.deltap_ = np.array([self.lattice.get_p_lattice()[i+1] - self.lattice.get_p_lattice()[i]
+                                 for i in range(len(self.lattice.get_p_lattice()) - 1)])
 
         self.qs = Qs
         self.alphas = 0.1 # Estimation for high momentum particles
@@ -49,8 +50,12 @@ class Evolution():
                 # Take 0.1 as the amplitude to avoid Bose-Einstein condensation
                 func.append(0.1)
             else:
+                #Uncomment this for a step function distribution
                 func.append(0)
-                #func.append(0.1 * np.exp(-1000*(p - self.qs)**2))
+
+                #Uncomment this for a step function distribution with a smooth gaussian decay
+                #sigma = 0.03
+                #func.append(0.1 * np.exp(-(p - self.qs)**2/(2*sigma**2)))
 
         func = np.array(func)
         numb = np.array([self.lattice.number(i, func) for i in range(len(self.lattice.get_lattice())-1)])
@@ -67,33 +72,50 @@ class Evolution():
             - float with the Ia integral result
             - float with the Ib integral result
             - float with the T_start result
+            - numpy array with the current
         """
 
         # Useful quantities
 
-        Ia = sum(self.deltap * self.lattice.get_p_lattice()**2 * self.function * (1 + self.function)) / (2 * np.pi ** 2)
-        
-        
-        
-        Ib = 2 * sum(self.deltap * self.lattice.get_p_lattice() * self.function) / (2 * np.pi ** 2)
-        
-        
+        Ia = np.sum(self.deltap * self.lattice.get_p_lattice()**2 * self.function * (1 + self.function)) / (2 * np.pi ** 2)
+        Ib = 2 * np.sum(self.deltap * self.lattice.get_p_lattice() * self.function) / (2 * np.pi ** 2)
         T_star = Ia / Ib
 
         # Debye screening mass
-        mD2 = 2 * self.alphas * self.Nc * Ib / np.pi
+        #mD2 = 2 * self.alphas * self.Nc * Ib / np.pi
 
         # Jet quenching parameter
         #p_t = 1
         #self.qhat = 4 * self.alphas**2 * self.Nc**2 * Ia * np.log(p_t / mD2) / np.pi
         self.qhat = Ia
 
-        mom_deriv = np.array([(self.function[i+1] - self.function[i]) / self.deltap_[i] for i in range(len(self.function) - 1)])
+        #mom_deriv = np.array([(self.function[i+1] - self.function[i]) / self.deltap_[i] for i in range(len(self.deltap_))])
 
-        deriv = self.qhat * self.lattice.get_p_lattice()[:-1]**2 * (
-                mom_deriv + self.function[:-1] * (1 + self.function[:-1]) / T_star) / 4
+        #Using a second order derivative
+        mom_deriv = (self.function[2:] - self.function[:-2]) / (self.deltap_[:-1] + self.deltap_[1:])
+        #mom_deriv = np.array([(self.function[i+1] - self.function[i-1]) / (self.deltap_[i-1] + self.deltap_[i]) for i in range(1, len(self.deltap_))]) # Second order derivative
+        #mom_deriv = np.insert(mom_deriv, 0, (self.function[1] - self.function[0]) / self.deltap_[0])
 
-        return deriv, Ia, Ib, T_star
+        Jp = self.qhat * (mom_deriv + self.function[1:-1] * (1 + self.function[1:-1]) / T_star) / 4
+        #Jp = np.append(Jp, 0) # Suppose at p = inf, Jp = 0
+
+        """Regular interpolation"""
+        Jp_ = np.interp(self.lattice.get_lattice()[1:-1],
+                        self.lattice.get_p_lattice()[1:-1],
+                        Jp)
+
+        """BSpline interpolation"""
+        #tck = interpolate.splrep(self.lattice.get_p_lattice(), Jp)
+        #spline  = interpolate.BSpline(*tck)
+
+        #Jp_ = spline(self.lattice.get_lattice()[1:-1])
+        Jp_ = np.insert(Jp_, 0, 0) # No current at origin
+        Jp_ = np.append(Jp_, 0)
+
+        deriv = self.lattice.get_lattice()[1:]**2 * Jp_[1:] - self.lattice.get_lattice()[:-1]**2 * Jp_[:-1]
+        self.Jp = Jp_
+
+        return deriv, Ia, Ib, T_star, Jp_
 
     def next_step(self):
 
@@ -102,76 +124,115 @@ class Evolution():
         :return:
             - numpy array with the new distribution function values
             - numpy array with the new number density values
-            - list containing the values of Ia, Ib and T_star for the current time step.
+            - list containing the values of Ia, Ib, T_star the current Jp and the derivative deriv for the current time step.
         """
 
         # Compute the new number distribution and add the boundary condition n(infinity) = f(infinity) = 0
-        deriv, Ia, Ib, T_star = self.derivative()
-        number_new = self.number[:-1] + self.deltat * deriv
-        number_new = np.append(number_new, 0)
+        deriv, Ia, Ib, T_star, Jp = self.derivative()
+        number_new = self.number + self.deltat * deriv
 
         # Recover the new values for the function distribution
-        function_new = np.array(
-            [number_new[i] * (6 * np.pi**2) / (self.lattice.get_lattice()[i+1] ** 3 - self.lattice.get_lattice()[i] ** 3)
-             for i in range(len(number_new))])
+        function_new = number_new * (6 * np.pi**2) / (self.lattice.get_lattice()[1:]**3 - self.lattice.get_lattice()[:-1]**3)
+        #function_new = np.array(
+        #    [number_new[i] / (self.lattice.get_lattice()[i+1] ** 3 - self.lattice.get_lattice()[i] ** 3)
+        #     for i in range(len(number_new))]) * (6 * np.pi**2)
+        #function_new = np.append(function_new, function_new[-1])
 
         self.function = function_new
         self.number = number_new
 
-        return function_new, number_new, [Ia, Ib, T_star]
+        return function_new, number_new, [Ia, Ib, T_star, Jp, deriv]
 
-    def evolve(self, save=False, plot=False):
+    def evolve(self, save=False, all=True, plot=False):
 
         """
         Compute the time evolution of the number distribution
         :param save: int, number of iteration after which data is saved. If False, no data is saved
+        :param all: boolean, if True, it will save the values during the evolution
         :param plot: if True, distribution is plot after each time step
         :return:
         """
 
+        plt.figure(figsize=(15, 10))
         for i in range(self.nt):
 
             fn, nn, adi = self.next_step()
 
             if save and i % save == 0:
-                self.save(i, additional=adi)
+                self.save(i, additional=adi, all=all)
 
             if plot:
                 self.plot_evo(i)
 
         return
 
-
-    def save(self, iter, additional=False):
+    def save(self, iter, additional=False, all=True):
 
         """
         Saves the data of the current step in different text files
         :param iter: current iteration of the evolution
-        :param additional: additional parameters to save. Must be False or a [Ia, Ib, T_star] list
+        :param additional: additional parameters to save. Must be False or a [Ia, Ib, T_star, Jp, deriv] list
         :return:
         """
 
-        os.makedirs('data/function', exist_ok=True)
-        os.makedirs('data/number', exist_ok=True)
+        if all and not additional:
 
-        np.savetxt('data/function/iteration_%i.txt' %iter, self.function)
-        np.savetxt('data/number/iteration_%i.txt' %iter, self.number)
+            os.makedirs('data/function', exist_ok=True)
+            os.makedirs('data/number', exist_ok=True)
+            os.makedirs('data/Jp', exist_ok=True)
+            os.makedirs('data/deriv', exist_ok=True)
 
-        if additional:
+            np.savetxt('data/function/iteration_%i.txt' %iter, self.function)
+            np.savetxt('data/number/iteration_%i.txt' %iter, self.number)
 
-            Ia, Ib, T_star = additional
+        elif all and additional:
+
+            os.makedirs('data/function', exist_ok=True)
+            os.makedirs('data/number', exist_ok=True)
+            os.makedirs('data/Jp', exist_ok=True)
+            os.makedirs('data/deriv', exist_ok=True)
+
+            np.savetxt('data/function/iteration_%i.txt' %iter, self.function)
+            np.savetxt('data/number/iteration_%i.txt' %iter, self.number)
+
+            Ia, Ib, T_star, Jp, deriv = additional
             number = sum(self.number)
             energy = sum([self.lattice.energy(i, self.function) for i in range(len(self.function))])
             entropy = sum([self.lattice.entropy(i, self.function) for i in range(len(self.function))])
 
             integrals = open('integrals.txt', 'a')
             stats = open('stats.txt', 'a')
+            chem_pot = open('chem_pot.txt', 'a')
 
             integrals.write('%.16e %.16e %.16e\n' %(Ia, Ib, T_star))
             stats.write('%.16e %.16e %.16e\n' %(number, energy, entropy))
+            chem_pot.write('%.16e\n' %(- T_star * np.log(1 + 1/self.function[0])))
 
             integrals.close()
             stats.close()
+            chem_pot.close()
+
+            np.savetxt('data/Jp/iteration_%i.txt' %iter, Jp)
+            np.savetxt('data/deriv/iteration_%i.txt' %iter, deriv)
+
+        elif not all and additional:
+
+            Ia, Ib, T_star, Jp, deriv = additional
+            number = sum(self.number)
+            energy = sum([self.lattice.energy(i, self.function) for i in range(len(self.function))])
+            entropy = sum([self.lattice.entropy(i, self.function) for i in range(len(self.function))])
+
+            integrals = open('integrals.txt', 'a')
+            stats = open('stats.txt', 'a')
+            chem_pot = open('chem_pot.txt', 'a')
+
+            integrals.write('%.16e %.16e %.16e\n' %(Ia, Ib, T_star))
+            stats.write('%.16e %.16e %.16e\n' %(number, energy, entropy))
+            chem_pot.write('%.16e\n' %(- T_star * np.log(1 + 1/self.function[0])))
+
+            integrals.close()
+            stats.close()
+            chem_pot.close()
 
         return
 
@@ -189,10 +250,10 @@ class Evolution():
         output = open('conservation.txt', 'w')
 
         numb = max(number) - min(number)
-        output.write('Particle number was conserved but %.16f particles, a %f %%\n' %(numb, 100*numb/max(number)))
+        output.write('Particle number was conserved but %.16e particles, a %.16f %%\n' %(numb, 100*numb/number[0]))
 
         ener = max(energy) - min(energy)
-        output.write('Energy density was conserved but %.16f particles, a %f %%\n' % (ener, 100*ener/max(energy)))
+        output.write('Energy density was conserved but %.16e , a %.16f %%\n' % (ener, 100*ener/energy[0]))
 
         output.close()
 
@@ -200,12 +261,13 @@ class Evolution():
 
     def plot_evo(self, iter):
 
-        if iter % 10 == 0:
+        if iter % 10000 == 0:
             plt.clf()
-            plt.plot(self.function, 'r', label='Distribution')
-            plt.plot(self.number, 'b', label='Number')
-            plt.legend(loc='upper right', title='Iteration: %i' %iter)
-            plt.pause(0.1)
+            plt.plot(self.function, 'r.', label='Distribution')
+            #plt.xscale('log')
+            #plt.plot(self.number, 'b', label='Number')
+            plt.grid()
+            plt.legend(loc='upper right', title='Iteration: %i\n Minimum = %f' %(iter, min(self.function)))
+            plt.pause(0.5)
 
         return
-
